9a0c4e350907b97d4d4a94b5dca3ef5f5e993b26
Taehoon Kim <u1260161@umail.utah.edu>
2025-02-14 13:45:57 -0700

    dim is working, seperation of order needed

--- Src/lab3.c
+++ Src/lab3.c
@@ -2,9 +2,7 @@
 #include "hal_gpio.h"
 #include <stm32f0xx_hal.h>
 
-/**
- * @brief  Initialize GPIO for TIM3 PWM on PC6 and PC7.
- */
+
 void GPIO_Init_TIM3_PWM(void)
 {
     // Enable GPIOC clock
@@ -14,8 +12,9 @@ void GPIO_Init_TIM3_PWM(void)
     GPIOC->MODER &= ~((3 << (2 * 6)) | (3 << (2 * 7))); // Clear mode bits
     GPIOC->MODER |= ((2 << (2 * 6)) | (2 << (2 * 7)));  // Set to AF mode
 
-    // Set PC6 (TIM3_CH1) and PC7 (TIM3_CH2) to AF0
-    GPIOC->AFR[0] &= ~((0xF << (4 * 6)) | (0xF << (4 * 7))); // AF0 (TIM3)
+    // Set PC6 (TIM3_CH1) and PC7 (TIM3_CH2) to AF1
+    GPIOC->AFR[0] &= ~((0xF << (4 * 6)) | (0xF << (4 * 7))); 
+    // GPIOC->AFR[0] |= ((1 << (4 * 6)) | (1 << (4 * 7))); // AF1 (TIM3_CH1, TIM3_CH2)
 }
 
 /**
@@ -32,10 +31,11 @@ void TIM3_PWM_Init(void)
     TIM3->PSC = 99;
     TIM3->ARR = 99;
 
-    // Configure TIM3 Channel 1 and 2 for PWM Mode 1 (OCxM = 110)
+    // Configure TIM3 Channel 1 to PWM Mode 2 (OC1M = 111)
+    // Configure TIM3 Channel 2 to PWM Mode 1 (OC2M = 110)
     TIM3->CCMR1 &= ~(TIM_CCMR1_OC1M | TIM_CCMR1_OC2M); // Clear bits
-    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC1M_Pos); // CH1: PWM Mode 1
-    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC2M_Pos); // CH2: PWM Mode 1
+    TIM3->CCMR1 |= (7 << TIM_CCMR1_OC1M_Pos); // CH1: PWM Mode 2 (111)
+    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC2M_Pos); // CH2: PWM Mode 1 (110)
 
     // Enable Output Compare Preload for both channels
     TIM3->CCMR1 |= TIM_CCMR1_OC1PE | TIM_CCMR1_OC2PE;
@@ -44,8 +44,8 @@ void TIM3_PWM_Init(void)
     TIM3->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
 
     // Set initial duty cycle to 20%
-    TIM3->CCR1 = TIM3->ARR / 50; // 20% duty cycle for CH1
-    TIM3->CCR2 = TIM3->ARR / 50; // 20% duty cycle for CH2
+    TIM3->CCR1 = (20 * TIM3->ARR) / 100; // 20% duty cycle for CH1 (PWM Mode 2)
+    TIM3->CCR2 = (20 * TIM3->ARR) / 100; // 20% duty cycle for CH2 (PWM Mode 1)
 
     // Enable TIM3 counter
     TIM3->CR1 |= TIM_CR1_CEN;
@@ -64,18 +64,18 @@ int lab3_main(void)
     TIM3_PWM_Init();
 
     while (1)
-    {  
+    {
         // Smoothly increase and decrease brightness of the LED
         for (uint16_t i = 0; i <= 100; i += 5)
         {
-            TIM3->CCR1 = (i * TIM3->ARR) / 100;  // CH1 (Red LED)
-            TIM3->CCR2 = ((100 - i) * TIM3->ARR) / 100; // CH2 (Blue LED)
+            TIM3->CCR1 = (i * TIM3->ARR) / 100;  // CH1 (Red LED, PWM Mode 2)
+            TIM3->CCR2 = ((100 - i) * TIM3->ARR) / 100; // CH2 (Blue LED, PWM Mode 1)
             HAL_Delay(50);
         }
         for (uint16_t i = 100; i >= 0; i -= 5)
         {
-            TIM3->CCR1 = (i * TIM3->ARR) / 100;  // CH1 (Red LED)
-            TIM3->CCR2 = ((100 - i) * TIM3->ARR) / 100; // CH2 (Blue LED)
+            TIM3->CCR1 = (i * TIM3->ARR) / 100;  // CH1 (Red LED, PWM Mode 2)
+            TIM3->CCR2 = ((100 - i) * TIM3->ARR) / 100; // CH2 (Blue LED, PWM Mode 1)
             HAL_Delay(50);
         }
     }
